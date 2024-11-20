b3e34af60a1a56d9729e5ad9095961fbd35ed867
Sobuz_Nub <khanshobuz99@gmail.com>
2024-11-20 11:39:09 +0600

    testimonials section added

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,5 +1,6 @@
 import FeaturedProducts from "./HomeComponents/FeaturedProducts";
 import Hero from "./HomeComponents/Hero";
+import Testimonials from "./HomeComponents/Testimonials";
 
 
 const Home = () => {
@@ -9,6 +10,9 @@ const Home = () => {
             <div className="container mx-auto">
                 <p className="text-3xl font-bold font-mono mt-12 my-12 text-center">Featured Products</p>
                 <FeaturedProducts></FeaturedProducts>
+            </div>
+            <div className="container mx-auto">
+                <Testimonials></Testimonials>
             </div>
         </div>
     );
